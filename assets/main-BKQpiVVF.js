50393657c7bf3ca1769033229a75b52da7ed0a28
egodiseño <egodiseno@gmail.com>
2024-09-16 01:05:35 -0600

    cambios mayores

--- assets/main-BKQpiVVF.js
+++ assets/main-BKQpiVVF.js
@@ -84,8 +84,8 @@
   <div class="container mx-auto flex px-5 py-0 items-center justify-center flex-col">
     
     <!-- Imagen con descripción accesible -->
-    <img loading="lazy" class="lg:w-1/4 md:w-3/6 w-5/6 mb-10 object-cover object-center" 
-         alt="Logotipo de GGB Servicios" 
+    <img class="lg:w-1/4 md:w-3/6 w-5/6 mb-10 object-cover object-center" 
+    rel="preload" alt="Logotipo de GGB Servicios" 
          src="./images/ggblogo.svg">
     
     <!-- Contenedor de texto central -->
@@ -729,4 +729,4 @@
       allowfullscreen="" 
       loading="lazy">
     </iframe>`}document.addEventListener("DOMContentLoaded",function(){const t=document.getElementById("map-container");t?(t.innerHTML=C(),console.log("Iframe inserted successfully in desktop view")):console.log("Map container not found in desktop view");const o=document.getElementById("map-container-mobile");o?(o.innerHTML=C(),console.log("Iframe inserted successfully in mobile view")):console.log("Map container mobile not found")});document.addEventListener("DOMContentLoaded",()=>{var g,h;const t=document.getElementById("modal-privacidad"),o=document.getElementById("modal-terminos"),n=document.getElementById("open-privacidad-mobile"),l=document.getElementById("open-terminos-mobile"),e=document.getElementById("open-privacidad-desktop"),s=document.getElementById("open-terminos-desktop");async function r(a,i){try{const c=await fetch(i);if(!c.ok)throw new Error("Error de red");const E=await c.text();a.querySelector(".modal-body").innerHTML=E}catch(c){console.error("Error al cargar el contenido del modal:",c),a.querySelector(".modal-body").innerHTML="<p>Error al cargar el contenido.</p>"}}function b(a,i){r(a,i),a.classList.remove("hidden"),document.body.style.overflow="hidden"}function p(a){const i=a.currentTarget.getAttribute("data-close");document.getElementById(i).classList.add("hidden"),document.body.style.overflow=""}n==null||n.addEventListener("click",a=>{a.preventDefault(),b(t,"aviso-privacidad.html")}),l==null||l.addEventListener("click",a=>{a.preventDefault(),b(o,"terminos-condiciones.html")}),e==null||e.addEventListener("click",a=>{a.preventDefault(),b(t,"aviso-privacidad.html")}),s==null||s.addEventListener("click",a=>{a.preventDefault(),b(o,"terminos-condiciones.html")}),(g=document.querySelector('button[data-close="modal-privacidad"]'))==null||g.addEventListener("click",p),(h=document.querySelector('button[data-close="modal-terminos"]'))==null||h.addEventListener("click",p),document.querySelectorAll(".modal").forEach(a=>{a.addEventListener("click",i=>{i.target===a&&p(i)})})});
-//# sourceMappingURL=main-CVk3y5Tr.js.map
+//# sourceMappingURL=main-BKQpiVVF.js.map
